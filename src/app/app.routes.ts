982d119ffe81637274b908cb50523e115d7833f0
Jamith Nimantha <jamithnimantha@gmail.com>
2025-12-31 03:33:08 +0530

    fix: updated info

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
-import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
   { 
